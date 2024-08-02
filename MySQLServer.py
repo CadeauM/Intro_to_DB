ac705c8fdbab09090b4ac8559f0c9dd0c90506b0
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-08-02 20:56:38 +0200

    add the database for book store

---
+++ MySQLServer.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host = '127.0.0.1'
+            user = 'root'
+            password = 'Cadeau@0707'
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+            cursor.execute(create_db_query)
+            print("Database 'alx_book_store' created successfully!")
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+create_database()  #calling the function
