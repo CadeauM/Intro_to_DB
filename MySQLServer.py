42d94233cb3aff8b4577342b82f419463e095fcd
Cadeau <Cadeaudedeu@gmail.com>
2025-07-21 00:18:26 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,7 @@ def create_database():
         connection = mysql.connector.connect(
             host = '127.0.0.1'
             user = 'root'
-            password = 'Cadeau@0707'
+            password = ''
         )
         if connection.is_connected():
             cursor = connection.cursor()
